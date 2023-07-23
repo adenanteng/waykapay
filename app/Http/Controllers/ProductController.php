7685f1b3891123baa89f3841dc13750c91205a9b
aden anteng <hey@anteng.co>
2023-07-23 12:46:42 +0700

    digiflazz

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -31,7 +31,7 @@ class ProductController extends Controller
             'testing' => true
         ]);
 
-        dd($response->object());
+//        dd($response->object());
 
         if ($response->successful()) {
 //            dd($response->object()->data->status);
