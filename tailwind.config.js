c12d57c116e5fb5c0530495f2338f085abc22459
aden anteng <hey@anteng.co>
2023-07-26 21:51:41 +0700

    huh

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
     darkMode: 'class',
     theme: {
         colors: {
-            primary: colors.red,
+            primary: colors.amber,
             transparent: 'transparent',
             current: 'currentColor',
             neutral: colors.neutral,
