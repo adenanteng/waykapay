61d21868ec49f3558022c58c407bc208a815e5ec
aden anteng <hey@anteng.co>
2023-08-04 09:55:01 +0700

    improvements

--- resources/js/Pages/Carousel/Index.vue
+++ resources/js/Pages/Carousel/Index.vue
@@ -43,12 +43,12 @@ const logout = () => {
         <div class="grid grid-cols-1 gap-4 sm:grid-cols-3">
 
             <template v-for="data in props.carousel" >
-                <div class="relative items-center space-x-3">
-                        <img class="h-auto w-full aspect-video object-cover rounded-3xl" :src="data.media[0].original_url" alt="">
+                <div class="relative items-center bg-white bg-opacity-50 border border-gray-300 shadow-lg rounded-3xl">
+                        <img class="h-auto w-full aspect-video object-cover rounded-t-3xl" :src="data.media[0].original_url" alt="">
 
                         <Link :href="route('carousel.edit', data)" class="focus:outline-none text-left">
                             <span class="absolute inset-0" aria-hidden="true"></span>
-                            <p class="text-sm font-medium text-gray-900">{{ data.href }}</p>
+                            <p class="text-sm font-medium text-gray-900 px-3 py-2">{{ data.href }}</p>
                         </Link>
 
                 </div>
