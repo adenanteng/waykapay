ab717569ce6d286ead8e823a0b34b4a1fb8a4e61
aden anteng <hey@anteng.co>
2023-09-19 14:44:57 +0700

    chapter four

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -14,12 +14,11 @@ class WebHookController extends Controller
      */
     public function webhookHandlerFlipAcceptPayment(Request $request){
 
-        $transaction = Transaction::where('order_id', $request['link_id'])->first();
-        dd($transaction);
+        $transaction = Transaction::where('order_id', $request['bill_link_id'])->first();
         $user = User::where('id', $transaction['user_id'])->first();
 
         if ($transaction->status_id != Transaction::SUCCESS) {
-            switch($request['bill_payment']['status']) {
+            switch($request['status']) {
                 case ('SUCCESSFUL'):
                     $user->deposit($transaction->amount);
                     $status_id = Transaction::SUCCESS;
