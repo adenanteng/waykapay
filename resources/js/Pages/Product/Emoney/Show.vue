caea4027d0b0a1914ed9ece9eb1f3be48293a36a
aden anteng <hey@anteng.co>
2023-11-15 06:52:59 +0700

    redis

--- resources/js/Pages/Product/Emoney/Show.vue
+++ resources/js/Pages/Product/Emoney/Show.vue
@@ -167,7 +167,7 @@ const closeModal = () => {
             </template>
 
             <template v-else>
-                {{ props.response.data }}
+                {{ props.response }}
             </template>
 
 <!--            <template v-else-if="props.response.data?.rc==83">-->
