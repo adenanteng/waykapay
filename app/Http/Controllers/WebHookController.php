d4f3882bd92f1d6a468153668e4b8409b2d85837
aden anteng <hey@anteng.co>
2023-11-21 14:58:22 +0700

    print

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -14,11 +14,12 @@ class WebHookController extends Controller
      * @return \Illuminate\Http\JsonResponse
      */
     public function webhookHandlerOkeConnect(Request $request){
-        Log::debug($request);
+//        Log::debug($request);
 
         $transaction = Transaction::where('desc', $request['merchantOrderId'])->first();
         $user = User::where('id', $transaction['user_id'])->first();
 
+//        dd($transaction);
         if ($transaction->status_id != Transaction::SUCCESS) {
             switch($request['status']) {
                 case ('SUCCESS'):
