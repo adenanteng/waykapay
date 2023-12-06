4e82e603249521d6b470210a3a6c1799f924e178
aden anteng <hey@anteng.co>
2023-12-06 08:42:55 +0700

    paramore

--- app/Helpers/Helper.php
+++ app/Helpers/Helper.php
@@ -120,7 +120,7 @@ class Helper
 //        dd($token, $msg, json_encode($array));
 
         $response = Http::post('https://api.pushy.me/push?api_key=c6c48c9d9c6de34d676a6ba63ca60d8fd6437b970e2f82facb9c6540918b6dd0', [
-            json_encode($array),
+            $array,
         ]);
 
 
