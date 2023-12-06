a189fb0ae8030a94778877cab215d6d5b3c44010
aden anteng <hey@anteng.co>
2023-12-06 10:53:45 +0700

    die

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -50,18 +50,18 @@ const onSlideChange = (swiper) => {
     // console.log(swipeMenu);
 };
 
-function halo() {
-    try {
-        // eslint-disable-next-line no-undef
-        if (AndroidBridge) {
-            // eslint-disable-next-line no-undef
-            console.log("Trying to call AndroidBridge.sendDataToKotlin");
-            AndroidBridge.sendDataToKotlin("Data yang ingin Anda bagikan");
-        }
-    } catch (error) {
-        console.error(error.message);
-    }
-}
+// function halo() {
+//     try {
+//         // eslint-disable-next-line no-undef
+//         if (AndroidBridge) {
+//             // eslint-disable-next-line no-undef
+//             console.log("Trying to call AndroidBridge.sendDataToKotlin");
+//             AndroidBridge.sendDataToKotlin("Data yang ingin Anda bagikan");
+//         }
+//     } catch (error) {
+//         console.error(error.message);
+//     }
+// }
 
 
 
