ac978556fd0bea173eedf083f3d39b20fd02a470
aden anteng <hey@anteng.co>
2023-12-11 09:29:49 +0700

    notification

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -227,9 +227,12 @@ class WebHookController extends Controller
 //            $device->delete();
 //        }
         $user = User::where('slug', Req::input('user'))->first();
-        $user->update([
-            'device_token' => Req::input('token'),
-        ]);
+        if ($user) {
+            $user->update([
+                'device_token' => Req::input('token'),
+            ]);
+        }
+
 
         return response()->json('ok');
     }
