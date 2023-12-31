7abfc4fd7f0b6318016a98caa09a547e03b94ba7
aden anteng <hey@anteng.co>
2023-12-31 09:46:29 +0700

    many many many updates

--- app/Http/Controllers/WebHookController.php
+++ app/Http/Controllers/WebHookController.php
@@ -149,7 +149,7 @@ class WebHookController extends Controller
     public function webhookHandlerDigiflazz(Request $request){
         Log::info($request->getContent());
 //        Log::debug(json_decode($request->getContent()));
-        $anj = $request->getContent();
+        $anj = $request->all();
 
         $transaction = Transaction::where('order_id', $anj->data->ref_id)->first();
         $user = User::where('id', $transaction['user_id'])->first();
