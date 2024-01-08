8cd0e34ffcedb7111bc33b93d5982d8e69b6f664
aden anteng <hey@anteng.co>
2024-01-08 10:58:16 +0700

    optimization

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -50,9 +50,9 @@ class HandleInertiaRequests extends Middleware
 
         return array_merge(parent::share($request), [
 //            'appSetting' => AppSetting::get()->first(),
-            'appSetting' => Helper::api(),
+//            'appSetting' => Helper::api(),
 //            'fee' => fn () => Helper::api()->fees,
-            'digiflazz_saldo' => Helper::api()->digiflazz_saldo ?? 0,
+//            'digiflazz_saldo' => Helper::api()->digiflazz_saldo ?? 0,
         ]);
     }
 }
